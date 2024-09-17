95f31b37198d93eea76ec9c23d79aad11676ce8a
jcesarmobile <jcesarmobile@gmail.com>
2024-09-17 18:59:43 +0200

    initial commit

---
+++ app/src/main/java/com/jcesarmobile/talkbackbug/MainActivity.java
@@ -0,0 +1,32 @@
+package com.jcesarmobile.talkbackbug;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        WebView webView = findViewById(R.id.webview);
+        WebView.setWebContentsDebuggingEnabled(true);
+        WebSettings webViewSettings = webView.getSettings();
+        webViewSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://tea-taster-ng.web.app/");
+    }
+}
